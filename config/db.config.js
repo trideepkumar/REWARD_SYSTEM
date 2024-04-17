face09dc7333e5d6a0612c27b4216d1911d6b22d
Trideep <118520595+trideepkumar@users.noreply.github.com>
2024-04-17 12:21:07 +0530

    url changed

--- config/db.config.js
+++ config/db.config.js
@@ -1,6 +1,6 @@
 const dev = {
     env: "dev",
-    url: "mongodb+srv://trideepkumar111:trideep@123@cluster0.xp2proq.mongodb.net/",
+    url: "mongodb+srv://trideepkumar111:LjtSehM21Knui0YU@reward-system.ubl0wue.mongodb.net",
     local:"mongodb://127.0.0.1:27017/test"
 }
 
