042b486f0ecd2084a8a1701c68e7c7b47429e894
Trideep <118520595+trideepkumar@users.noreply.github.com>
2024-04-17 12:09:44 +0530

    atlas url changed

--- config/db.config.js
+++ config/db.config.js
@@ -1,11 +1,11 @@
 const dev = {
     env: "dev",
-    // url : "mongodb://localhost:27017/WELSPUN_STHITHI_REWARDS_DEV",
-    url: "mongodb+srv://trideepkumar111:trideep@123@cluster0.xp2proq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0 "
+    url: "mongodb+srv://trideepkumar111:trideep@123@cluster0.xp2proq.mongodb.net/REWARD_SETUP",
 }
 
-module.exports = (config) => {
-    if(config === "dev"){
-        return dev
-    }  
-} 
+export default (config) => {
+    if (config === "dev") {
+        return dev;
+    }
+};
+
