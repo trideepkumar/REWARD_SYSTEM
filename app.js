cfc22c9730f7806b62ca7a5696a773d41b182ed1
adharsh <adharshmr001@gmail.com>
2024-04-17 12:33:59 +0530

    server changes

--- app.js
+++ app.js
@@ -1,23 +1,31 @@
-
 require('dotenv').config()
 const express = require('express')
 const qrRouter = require('./routes/qrROuter.js')
 const connection = require('./config/connection.js')
-const {dev} = require('./config/db.config.js')
+const { config } = require('./config/db.config.js')
 
 const app = express()
 const PORT = process.env.PORT || 4000;
 
-app.use('/',qrRouter)
+app.use('/', qrRouter)
+
+async function startServer() {
+    const uri = process.env.NODE_ENV === "developement" ? config.dev : config.local
+    connection(uri).then(() => {
+        app.listen(PORT, () => {
+            console.log(`Server is running on port ${PORT}`);
+        });
+    })
+    .catch((err) => {
+        console.error(err);
+    })
+}
+
+startServer();
 
 
 
-connection(dev.url)
 
-
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
 
 
 
--- config/db.config.js
+++ config/db.config.js
@@ -1,8 +1,8 @@
-const dev = {
+const config = {
     env: "dev",
-    url: "mongodb+srv://trideepkumar111:LjtSehM21Knui0YU@reward-system.ubl0wue.mongodb.net",
+    dev: "mongodb+srv://trideepkumar111:LjtSehM21Knui0YU@reward-system.ubl0wue.mongodb.net",
     local:"mongodb://127.0.0.1:27017/test"
 }
 
 
-module.exports = { dev }
+module.exports = { config }
