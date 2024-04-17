9660246349637cbfefa9af04a8b855822797768e
Trideep <118520595+trideepkumar@users.noreply.github.com>
2024-04-17 12:19:17 +0530

    change in controller

--- controllers/qrController.js
+++ controllers/qrController.js
@@ -1,6 +1,6 @@
 const  generateQr = async(req,res)=>{
      try{
-         
+         console.log(req.body)
      }catch(err){
         console.log(err)
         res.status(500).send('Something error happened!!')
