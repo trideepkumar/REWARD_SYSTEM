4ab1e48b1328a4e12306d1882ebf1ca6d7681ed7
adharsh <adharshmr001@gmail.com>
2024-04-18 10:01:05 +0530

    img updates

--- utils/generateQRCode.js
+++ utils/generateQRCode.js
@@ -1,27 +1,48 @@
+// const qr = require('qrcode');
+// const fs = require('fs');
+
+// // Function to generate QR code
+
+// async function generateQRCode(text) {
+//     try {
+//         // Generate QR code
+//         const qrData = await qr.toDataURL(text);
+
+//         // Convert base64 data to buffer
+//         const qrBuffer = Buffer.from(qrData.split(',')[1], 'base64');
+//         const fileName =`${Date.now()}.png`;
+//         // Save buffer to file
+//         fs.writeFileSync(`public/${fileName}`, qrBuffer);
+
+//         console.log('QR code saved successfully!');
+//         return fileName;
+//     } catch (error) {
+//         console.error('Error generating QR code:', error);
+//     }
+// }
+
+
+// // generateQRCode(qrText, fileName);
+
+// module.exports = generateQRCode;
+
 const qr = require('qrcode');
-const fs = require('fs');
 
-// Function to generate QR code
-
+// Function to generate QR code as base64
 async function generateQRCode(text) {
     try {
-        // Generate QR code
+        // Generate QR code as base64
         const qrData = await qr.toDataURL(text);
-
-        // Convert base64 data to buffer
-        const qrBuffer = Buffer.from(qrData.split(',')[1], 'base64');
-        const fileName =`${Date.now()}.png`;
-        // Save buffer to file
-        fs.writeFileSync(`public/${fileName}`, qrBuffer);
-
-        console.log('QR code saved successfully!');
-        return fileName;
+        // Extract base64 data
+        const base64Data = qrData.split(',')[1];
+        
+        console.log('QR code generated successfully as base64!');
+        
+        return base64Data;
     } catch (error) {
-        console.error('Error generating QR code:', error);
+        console.error('Error generating QR code as base64:', error);
+        throw error;
     }
 }
-
-
-// generateQRCode(qrText, fileName);
 
 module.exports = generateQRCode;
--- utils/generatreUniqueId.js
+++ utils/generatreUniqueId.js
@@ -3,7 +3,7 @@ const { v4: uuidv4 } = require('uuid');
 function generateShortUUID() {
     const fullUUID = uuidv4();
     const shortUUID = fullUUID.replace(/-/g, '').substr(0, 10);
-    return shortUUID;
+    return fullUUID;
 }
 
 module.exports = generateShortUUID;
