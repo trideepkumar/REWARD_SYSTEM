3ce1ffc5b62be88ed586d6ef8dd8cf74e7610d83
adharsh <adharshmr001@gmail.com>
2024-04-18 10:09:43 +0530

    uid  fix

--- utils/generatreUniqueId.js
+++ utils/generatreUniqueId.js
@@ -1,9 +1,13 @@
 const { v4: uuidv4 } = require('uuid');
 
 function generateShortUUID() {
+    // Generate a full UUID
     const fullUUID = uuidv4();
-    const shortUUID = fullUUID.replace(/-/g, '').substr(0, 10);
-    return fullUUID;
+    // Split the UUID into groups of 4 characters each
+    const groups = fullUUID.split('-');
+    // Concatenate the groups and separate them with "-"
+    const shortUUID = groups.join('').match(/.{1,4}/g).join('-').slice(0, 19);
+    return shortUUID;
 }
 
 module.exports = generateShortUUID;
